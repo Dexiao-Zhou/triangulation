312acc2b5da01f89e4a501127671ac2bcdc60675
Dexiao-Zhou <dexiao.zhou@qq.com>
2024-03-10 16:18:26 +0100

    更新了csv写入文件

--- DetectShapes.py
+++ DetectShapes.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import csv
+import os
 
 def detect_shapes(image_path):
     # 读取图片
@@ -45,7 +46,7 @@ def detect_shapes(image_path):
 
         contour_info['length'] = cv2.arcLength(cnt, True)
 
-        if contour_info['length'] < 1000:
+        if contour_info['length'] < 1000 and contour_info['length'] > 100:
             contour_info['coordinates'] = approx.tolist()  # 将坐标转换为列表形式
         else:
             continue
@@ -55,20 +56,50 @@ def detect_shapes(image_path):
         # 标记识别出的形状
         x = approx.ravel()[0]
         y = approx.ravel()[1] - 5
-        cv2.putText(img, f"{contour_info['shape']}", (x, y), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.5, color, 2)
+        cv2.putText(img, f"{contour_info['shape']}", (x, y),0.5, color, 2)
 
         # 选择性地展示处理后的图像
         cv2.drawContours(img, [cnt], 0, color, 2)  # 在原图上标记轮廓
         cv2.imshow("Contours Detected", img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        cv2.waitKey(1)
+        # cv2.destroyAllWindows()
 
     return contours_info
 
-# 调用函数
-image_path = '/Users/zhoudexiao/Downloads/triangulation/frames 4/frame_00000.jpg'  # 替换为你的图片路径
-contours_info = detect_shapes(image_path)
+def process_images_in_folder(folder_path, csv_file_path):
+    # 检查CSV文件是否已存在，以决定是否需要写入头部
+    file_exists = os.path.isfile(csv_file_path)
 
-for contour in contours_info:
-    print(contour)
+    with open(csv_file_path, 'a', newline='') as csvfile:
+        fieldnames = ['image', 'shape', 'length', 'point_x', 'point_y']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        if not file_exists:
+            writer.writeheader()
+        
+        filenames = sorted(os.listdir(folder_path))  # 这里添加了排序
+        for filename in filenames:
+            if filename.endswith(".jpg"):  # 检查文件扩展名
+                image_path = os.path.join(folder_path, filename)
+                contours_info = detect_shapes(image_path)
+                
+                for contour_info in contours_info:
+                    shape = contour_info['shape']
+                    length = contour_info['length']
+                    coordinates = contour_info['coordinates']
+                    
+                    # for point in contour_info['coordinates']:
+                    #     x, y = point[0], point[1]  # 假设每个坐标是一个包含两个元素的列表或元组
+                    #     writer.writerow({'image': filename, 'shape': shape, 'length': length, 'point_x': x, 'point_y': y})
+
+# 调用函数
+# image_path = '/Users/zhoudexiao/Downloads/triangulation/frames 4/frame_00000.jpg'  # 替换为你的图片路径
+# contours_info = detect_shapes(image_path)
+
+# # print一下当前的图片的数据信息
+# for contour in contours_info:
+#     print(contour)
+
+
+folder_path = '/Users/zhoudexiao/Downloads/triangulation/frames 3'  # 更新为你的文件夹路径
+csv_file_path = '/Users/zhoudexiao/Downloads/triangulation/frames 3/shapes_data.csv'
+process_images_in_folder(folder_path, csv_file_path)
